300c7abb2cf1cda449652442e7f89b2c7adfa57a
John Doe <johndoe@example.com>
2022-09-08 12:50:46 +0200

    harmadik feladat

---
+++ harmadik.js
@@ -0,0 +1,22 @@
+function hurra(){
+    console.log("Hurrá")
+}
+
+let time = setInterval(hurra,2000)
+
+//3a
+
+let a=0;
+function szamol(){
+
+
+console.log(a)
+a++;
+}
+setInterval(szamol,2000)
+
+
+
+
+
+    
